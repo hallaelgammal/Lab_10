89c5b9dc98412d38fe4cf33eaea1d3f39cf78870
Hana-Gamaleldin14 <es.hana.ahmed20231@alexu.edu.eg>
2025-12-04 00:09:50 +0200

    h

--- src/lab10/ValidationError.java
+++ src/lab10/ValidationError.java
@@ -4,10 +4,55 @@
  */
 package lab10;
 
+import java.util.List;
+
 /**
  *
  * @author Abdallah
  */
 public class ValidationError {
+    public enum Type {ROW, COL, BOX}
+    private final Type type;
+    private final int index;
+    private final int value;
+    private final List<String> locations;
     
+    public ValidationError(Type type, int index, int value, List<String> locations ) {
+        this.type = type;
+        this.index = index;
+        this.value = value;
+        this.locations = locations;
+    }
+    
+    public Type getType()
+    {
+        return type;
+    }
+    public int getIndex()
+    {
+        return index;
+    }
+    public int getValue()
+    {
+        return value;
+    }
+    public List<String> getLocations()
+    {
+        return locations;
+    }
+    
+    @Override
+    public String toString()
+    {
+       return String.format(
+               "%s %d, #%d, %s" ,
+               type.name(),
+               index,
+               value,
+               locations.toString()
+       );
+               
+    }
 }
+
+
